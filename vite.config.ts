01c1598021a01fdc3a257d2dc9df6595a42ce9ef
panawar <panawarhsb28@gmail.com>
2023-07-30 13:53:05 +0700

    chore: import absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@features': '/src/features',
+      '@utils': '/src/utils',
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@hooks': '/src/hooks',
+      '@context': '/src/context',
+      '@lib': '/src/lib',
+      '@service': '/src/service',
+      '@data': '/src/data',
+    },
+  },
 });
